34148341d75e3432f93141249442dd47e03abace
marily-gonzalez <marilyngonzalezgonzalez86@gmail.com>
2024-12-15 22:01:40 -0500

    Add files via upload

---
+++ proyecto_teatro_apolo-main/js/arreglos.js
@@ -0,0 +1,7 @@
+
+// document.addEventListener("DOMContentLoaded", function() {
+//     const button = document.querySelector("button");
+//     button.addEventListener("click", function() {
+//         alert("¡Gracias por inscribirte! Pronto nos pondremos en contacto contigo.");
+//     });
+// });
---
+++ proyecto_teatro_apolo-main/js/uniformes.js
@@ -0,0 +1,8 @@
+
+// document.querySelectorAll('.btn-uniforme').forEach(button => {
+//     button.addEventListener('click', () => {
+//       const targetId = button.dataset.target;
+//       const descripcionUniforme = document.querySelector(targetId);
+//       descripcionUniforme.style.display = 'block';
+//     });
+//   });
