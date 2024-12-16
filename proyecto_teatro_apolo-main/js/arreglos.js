c5839f6564d1c6e51ccc398da2424192ef5c8d8a
marily-gonzalez <marilyngonzalezgonzalez86@gmail.com>
2024-12-15 21:58:19 -0500

    Delete proyecto_teatro_apolo-main directory

--- proyecto_teatro_apolo-main/js/arreglos.js
+++
@@ -1,7 +0,0 @@
-
-// document.addEventListener("DOMContentLoaded", function() {
-//     const button = document.querySelector("button");
-//     button.addEventListener("click", function() {
-//         alert("¡Gracias por inscribirte! Pronto nos pondremos en contacto contigo.");
-//     });
-// });
--- proyecto_teatro_apolo-main/js/uniformes.js
+++
@@ -1,8 +0,0 @@
-
-// document.querySelectorAll('.btn-uniforme').forEach(button => {
-//     button.addEventListener('click', () => {
-//       const targetId = button.dataset.target;
-//       const descripcionUniforme = document.querySelector(targetId);
-//       descripcionUniforme.style.display = 'block';
-//     });
-//   });
